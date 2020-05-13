b7d38613898a2d075ff2939eecec57a7e1332663
c-thun <kapoor.aakash@outlook.com>
2020-05-14 04:46:50 +0530

    about page done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 (function() {
   var home_path = "snippets/home.html";
-  
+  var about_path = "snippets/about.html";
+
   var month_array = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var run_number = 0;
+
   var xkcd_utils = {
-    base_url: 'https://xkcd.now.sh/?comic',
-    currentXkcd: 'https://xkcd.now.sh/?comic=latest',
-    total_comics: 0
+    base_url: 'https://xkcd.now.sh/?comic=',
+    latestXkcd: 'https://xkcd.now.sh/?comic=latest',
+    total_comics: 0,
+    current_comic_number: 0
   };
 
   var insertHtml = function (selector, html) {
@@ -35,14 +39,17 @@
     insertHtml(selector, html);
   };
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // In the meantime show loading screen
-    showLoading("#main_content");
-
+  function loadComic(url) {
     // Asynchronously load the xkcd JSON
-    ajaxUtils.sendGetRequest(xkcd_utils.currentXkcd, function(xkcd_response) {
+    ajaxUtils.sendGetRequest(url, function(xkcd_response) {
       // Set the maximum number of comics as well
-      xkcd_utils.total_comics = xkcd_response.num;
+      if(run_number == 0) {
+        // For the first run only
+        xkcd_utils.total_comics = xkcd_response.num;
+        run_number += 1;
+        $("#forwdBtn").disabled = true;
+      }
+      xkcd_utils.current_comic_number = xkcd_response.num;
       var comic_date = xkcd_response.day + " " + month_array[xkcd_response.month-1] + ", " + xkcd_response.year;
       var img_url = xkcd_response.img;
       var comic_title = xkcd_response.safe_title;
@@ -59,7 +66,56 @@
       false);
     },
     true);
+  }
+
+  // For the initial landing experience
+  document.addEventListener("DOMContentLoaded", function() {
+    // In the meantime show loading screen
+    showLoading("#main_content");
+    loadComic(xkcd_utils.latestXkcd);    
   });
 
+  // Handling the back/forward buttons
+  document.querySelector("#backBtn").addEventListener("click", function(event) {
+    if(xkcd_utils.current_comic_number > 0) {
+      xkcd_utils.current_comic_number = xkcd_utils.current_comic_number - 1;
+      showLoading("#main_content");
+      loadComic(xkcd_utils.base_url+xkcd_utils.current_comic_number); 
+      $("#forwdBtn").disabled = false;
+    }
+    else {
+      $("#backBtn").disabled = true;
+    }
+    console.log(xkcd_utils);
+  });
+
+  document.querySelector("#forwdBtn").addEventListener("click", function(event) {
+    if(xkcd_utils.current_comic_number < xkcd_utils.total_comics) {
+      xkcd_utils.current_comic_number += 1;
+      showLoading("#main_content");
+      loadComic(xkcd_utils.base_url+xkcd_utils.current_comic_number); 
+    }
+    else {
+      $("#forwdBtn").disabled = true;
+    }
+    console.log(xkcd_utils);
+  });
+
+  // Load the About page
+  document.querySelector("#about_link").addEventListener("click", function(event) {
+    ajaxUtils.sendGetRequest(about_path, function(about_response) {
+      var main_div = document.querySelector("#main_content");
+      main_div.innerHTML = about_response;
+    },
+    false);
+    // Hide the lower navbar
+    $("#bottomNav").hide();
+  });
+
+  // Load the Home page
+  document.querySelector("#nav-link-home").addEventListener("click", function(event) {
+    showLoading("#main_content");
+    loadComic(xkcd_utils.latestXkcd);  
+  });
 
 })(window);
